ae27a67dc52cd631f4bff1cb80bbb78b74f3b193
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-12-22 18:05:27 +0100

    added dictionarty api integration
    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [Keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${Keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
