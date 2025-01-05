34145800c575b81bde15d2d61183e1a09e27f0cc
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2025-01-05 11:07:39 +0100

    display only the first definition, example, synonym

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,17 +6,17 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
-
-            <Synonyms synonyms={definition.synonyms} />
+      {props.meaning.definitions.length > 0 && (
+        <div>
+          <div className="definition">
+            {props.meaning.definitions[0].definition}
           </div>
-        );
-      })}
+
+          <div className="example">{props.meaning.definitions[0].example}</div>
+
+          <Synonyms synonyms={props.meaning.definitions[0].synonyms} />
+        </div>
+      )}
     </div>
   );
 }
